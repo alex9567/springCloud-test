056ab850adbddf7ed66b3e4fe2bd123a1bb6befd
chenhao <chenhao@qiekj.com>
2020-08-19 10:36:33 +0800

    添加注释

---
+++ service-hi/src/main/java/com/chen/hi/service/impl/ChenTestService2Impl.java
@@ -0,0 +1,28 @@
+package com.chen.hi.service.impl;
+
+import com.chen.openFeign.common.ResultTest;
+import com.chen.openFeign.service.ChenTestService2;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+public class ChenTestService2Impl implements ChenTestService2 {
+
+
+    @Override
+    public ResultTest<String> test1() {
+        return ResultTest.success("hello");
+    }
+
+    @Override
+    public ResultTest<String> test2() {
+        return ResultTest.success("success");
+    }
+
+    @Override
+    public ResultTest<String> test3() {
+        return ResultTest.success("success");
+    }
+}
