70ddf25d0120ff40bf6bf3a8fb9064631a6ff6c6
chen <715255995@qq.com>
2020-10-23 14:07:42 +0800

    添加注释

--- service-hi/src/main/java/com/chen/hi/service/impl/ChenTestService2Impl.java
+++ service-hi/src/main/java/com/chen/hi/service/impl/ChenTestService2Impl.java
@@ -13,16 +13,19 @@ public class ChenTestService2Impl implements ChenTestService2 {
 
     @Override
     public ResultTest<String> test1() {
+        log.info("test1");
         return ResultTest.success("hello");
     }
 
     @Override
     public ResultTest<String> test2() {
+        log.info("test2");
         return ResultTest.success("success");
     }
 
     @Override
     public ResultTest<String> test3() {
+        log.info("test3");
         return ResultTest.success("success");
     }
 }
